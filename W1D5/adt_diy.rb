25e6902a52f55cf8279b2b05cbef59ca0c54def8
christine.pham <cstinepham@gmail.com>
2018-10-26 01:05:01 -0700

    Add Map class

--- W1D5/adt_diy.rb
+++ W1D5/adt_diy.rb
@@ -35,3 +35,38 @@ class Queue
   end
 
 end
+
+#set(key, value), get(key), delete(key), show
+
+class Map
+  def initialize
+    @map_arr = Array.new
+
+  end
+
+  def set(key, value)
+    keys = []
+    @map_arr.each{|pair| keys << pair.first}
+    if keys.include?(key)
+      @map_arr.map do |pair|
+        pair.first == key ? pair = [key, value] : pair
+      end
+    else
+      @map_arr << [key, value]
+    end
+  end
+
+  def get(key)
+    @map_arr.each{|pair| return pair.last if pair.first == key}
+  end
+
+  def delete(key)
+    @map_arr.reject{|pair| pair.first == key}
+    key
+  end
+
+  def show
+    @map_arr
+  end
+
+end
