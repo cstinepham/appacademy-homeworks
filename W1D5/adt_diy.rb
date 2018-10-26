69a30391f33804edbc3ed84fd1ac047dff9f8dba
christine.pham <cstinepham@gmail.com>
2018-10-26 00:15:20 -0700

    Add Queue class

--- W1D5/adt_diy.rb
+++ W1D5/adt_diy.rb
@@ -16,3 +16,22 @@ class Stack
   end
 
 end
+
+class Queue
+  def initialize
+    @queue_arr = []
+  end
+
+  def enqueue(el)
+    @queue_arr << el
+  end
+
+  def dequeue
+    @queue_arr.shift
+  end
+
+  def peek
+    @queue_arr.first
+  end
+
+end
