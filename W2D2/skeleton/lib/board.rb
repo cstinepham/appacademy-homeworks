0ba8fca2f7e1ba4811ceef896647dbf541acaacc
christine.pham <cstinepham@gmail.com>
2018-11-03 16:18:39 -0700

    Implement #make_move to pass more specs

--- W2D2/skeleton/lib/board.rb
+++ W2D2/skeleton/lib/board.rb
@@ -1,68 +1,82 @@
 class Board
-  attr_accessor :cups
-
-  def initialize(name1, name2)
-    @cups = Array.new(14){[]}
-    @cups.each_with_index do |cup, i|
-      4.times{cup << :stone} if i != 6 && i != 13
+    attr_accessor :cups
+    
+    def initialize(name1, name2)
+        @cups = Array.new(14){[]}
+        @cups.each_with_index do |cup, i|
+            if (0..5).to_a.include?(i) || (7...13).to_a.include?(i)
+                4.times {cup << :stone}
+            end
+        end
+        @name1 = name1
+        @name2 = name2
+        @current_player_name = name1
     end
-
-  end
-
-  def place_stones
-
-  end
-
-  def valid_move?(start_pos)
-    raise 'Invalid starting cup' if start_pos > @cups.length-1 || start_pos < 0
-    raise 'Starting cup is empty' if @cups[start_pos].empty?
-
-  end
-
-  def make_move(start_pos, current_player_name)
-    if valid_move?(start_pos)
-      stones = @cups[start_pos]
-      @cups[start_pos] = nil
-      i = 0
-      until stones.empty?
-        @cups[start_pos+i] << :stone
-        stones.pop
-      end
+    
+    def place_stones
+        # helper method to #initialize every non-store cup with four stones each
     end
-  end
-
-  def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
-  end
-
-  def render
-    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
-    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
-    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
-    puts ""
-    puts ""
-  end
-
-  def one_side_empty?
-    return true if @cups[6].empty? || @cups[13].empty?
-    return false
-  end
-
-  def winner
-    if one_side_empty?
-      "#{name1}"
-    else
-      :draw
+    
+    def valid_move?(start_pos)
+        raise 'Invalid starting cup' if !(0..13).include?(start_pos)
+        raise 'Starting cup is empty' if @cups[start_pos].empty?
+        true
     end
-  end
-
-  def [](pos)
-    row,col = pos
-    @cups[row][col]
-  end
-
-  def []=(pos, value)
-    row,col = pos
-    @cups[row][col] = value
-  end
+    
+    def make_move(start_pos, current_player_name)
+        num_of_stones = @cups[start_pos].count
+        @cups[start_pos] = []
+        i = start_pos + 1
+        
+        if current_player_name == @name1
+            opponents_cup_ndx =  13
+            else
+            opponents_cup_ndx =  6
+        end
+        
+        until num_of_stones == 0
+            i = i % 13
+            if i != opponents_cup_ndx
+                @cups[i] << :stone
+                num_of_stones -= 1
+            end
+            i += 1
+        end
+        #render
+        next_turn(i-1)
+    end
+    
+    def next_turn(ending_cup_idx)
+        # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+        current_cup = @cups[ending_cup_idx]
+        if current_cup.empty?
+            return :switch
+        end
+    end
+    
+    def render
+        print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+        puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+        print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+        puts ""
+        puts ""
+    end
+    
+    
+    
+    
+    
+    
+    def one_side_empty?
+        player1_side_empty = @cups[0..5].all?{|cups| cups.empty?}
+        player2_side_empty = @cups[7..12].all?{|cups| cups.empty?}
+        player1_side_empty || player2_side_empty
+        
+    end
+    
+    def winner
+        six_stone_cup = [:stone, :stone, :stone, :stone, :stone, :stone]
+        return :draw if @cups[6] == six_stone_cup && @cups[13] == six_stone_cup
+        return @current_player_name
+    end
 end
