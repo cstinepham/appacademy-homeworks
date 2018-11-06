0cb645aa524eccb9c05e3f5e4b59975f94e234b3
christine.pham <cstinepham@gmail.com>
2018-11-06 00:46:05 -0800

    Add ORM methods to Playwright class

--- W3D2/skeleton/plays.rb
+++ W3D2/skeleton/plays.rb
@@ -83,3 +83,66 @@ class Play
     SQL
   end
 end
+
+
+# Playwright::all
+# Playwright::find_by_name(name)
+# Playwright#new (this is the initialize method)
+# Playwright#create
+# Playwright#update
+# Playwright#get_plays
+
+
+class Playwright
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def self.find_by_name(name)
+    result = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        playwrights
+      JOIN
+      WHERE
+        name = ?
+    SQL
+    return nil if result.length == 0
+    Playwright.new(result.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+  end
+
+end
