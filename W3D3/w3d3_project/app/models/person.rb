887939630cd942ad0245cc5e9048fba6df858400
christine.pham <cstinepham@gmail.com>
2018-11-07 00:23:06 -0800

    Add associations for Person and House

--- W3D3/w3d3_project/app/models/person.rb
+++ W3D3/w3d3_project/app/models/person.rb
@@ -1,3 +1,10 @@
 class Person  < ActiveRecord::Base
   validates :name, :house_id, present: true
+
+  belongs_to :house,
+    primary_key: :id
+    foreign_key: :house_id
+    class_name: 'House'
+  
+
 end
