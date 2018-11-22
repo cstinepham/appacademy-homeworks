b7899b7c9ec9e60587568732b9e5faa02c45e477
christine.pham <cstinepham@gmail.com>
2018-11-22 08:29:08 -0800

    Add #isPrime?

--- W5D4/javascript-intro.js
+++ W5D4/javascript-intro.js
@@ -11,8 +11,18 @@ function fizzBuzz(array) {
   const fizzBuzzArr = [];
   array.forEach(el =>
     if ((el % 3 === 0 && el % 5 != 0) || (el % 3 != 0 && el % 5 === 0)) {
-      fizzBuzzArr.push(el)
+      fizzBuzzArr.push(el);
     }
   );
   return fizzBuzzArr;
+}
+
+function isPrime(num) {
+  if (num < 2) { return false; }
+
+  for (let i = 0 ; i < num ; i+=1) {
+    if (num % i === 0) { return false; }
+  }
+  return true;
+
 }
