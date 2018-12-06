b9bca465f1fcd32a54f4b1f0a282587b29f22b25
christine.pham <cstinepham@gmail.com>
2018-12-06 08:00:52 -0800

    Add rainbowroutes to rainbow.jsx

--- W7D4/rainbowRoutes/components/rainbow.jsx
+++ W7D4/rainbowRoutes/components/rainbow.jsx
@@ -12,16 +12,17 @@ import Violet from './violet';
 
 class Rainbow extends React.Component {
   render() {
-    return (
-      <div>
+    return <div>
         <h1>Rainbow Router!</h1>
         {/* Your links should go here */}
 
         <div id="rainbow">
-          {/* Your routes should go here */}
+          <Route path="/red" component={Red} />
+          <Route path="/green" component={Green} />
+          <Route path="/blue" component={Blue} />
+          <Route path="/violet" component={Violet} />
         </div>
-      </div>
-    );
+      </div>;
   }
 };
 
